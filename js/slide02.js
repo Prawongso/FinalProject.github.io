f54a39f16446eacc45bf05c89bf5616a9c604982
webProgramYzu <115760872+webProgramYzu@users.noreply.github.com>
2022-12-14 06:58:52 +0800

    Add files via upload

---
+++ js/slide02.js
@@ -0,0 +1,44 @@
+var num = 0;
+var len = 2;
+var timer;
+autoPlay();
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+}
+function autoPlay() {
+    timer = setInterval(function () {
+        num++;
+        if (num >= len) {
+            num = 0;
+        }
+        plusSlides(1);
+    }, 2000);
+}
+function replay(){
+    clearInterval(timer);
+    autoPlay();
+}
